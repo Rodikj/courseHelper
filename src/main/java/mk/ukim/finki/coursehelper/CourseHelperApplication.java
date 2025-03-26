3ab1a86405e024baaa43b91ab6dc74dc5e8fc307
Rodikj_Ivica <ivicar159@gmail.com>
2025-03-26 20:57:27 +0100

    initial

---
+++ src/main/java/mk/ukim/finki/coursehelper/CourseHelperApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.coursehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseHelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/coursehelper/CourseHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.coursehelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
