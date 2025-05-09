848830ce053504f0a2c5f3c31c8462f984df8711
DoneDonev <donedonev15@gmail.com>
2025-05-09 18:52:07 +0200

    Tweaked the login/register page

--- frontend/src/components/LogIn/LoginForm.jsx
+++ frontend/src/components/LogIn/LoginForm.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { IoEyeOffOutline } from "react-icons/io5";
+import { IoEyeOutline } from "react-icons/io5";
 
 const LoginForm = () => {
     const [email, setEmail] = useState('');
@@ -6,15 +8,15 @@ const LoginForm = () => {
     const [showPassword, setShowPassword] = useState(false);
 
     return (
-        <form  className="space-y-4  align-middle">
-            <h2 className="text-3xl font-bold text-gray-800 mt-10 pt-5 mb-10">Log in</h2>
+        <form  className="space-y-4">
+            <h2 className="text-3xl font-bold text-[#2C2C2C] mb-6">Log in</h2>
             <div>
                 <input
                     type="email"
                     placeholder="Email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
-                    className="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
                     required
                 />
             </div>
@@ -24,24 +26,24 @@ const LoginForm = () => {
                     placeholder="Password"
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
-                    className="w-full p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    className="w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
                     required
                 />
                 <button
                     type="button"
                     onClick={() => setShowPassword(!showPassword)}
-                    className="absolute inset-y-0 right-3 flex items-center text-gray-500"
+                    className="absolute inset-y-0 right-3 flex items-center text-gray-600 hover:cursor-pointer"
                 >
-                    {showPassword ? 'Hide' : 'Show'}
+                    {showPassword ? <div className="text-[24px] font-bold"><IoEyeOutline /> </div>: <div className="text-[24px] font-bold"><IoEyeOffOutline /></div>}
                 </button>
             </div>
             <button
                 type="submit"
-                className="w-full bg-blue-500 hover:bg-blue-600 text-white font-semibold p-3 rounded-md transition duration-300"
+                className="w-full bg-[#2D84FF] hover:bg-blue-600 text-white font-semibold p-3 rounded-md transition duration-300 hover:cursor-pointer"
             >
                 Log In
             </button>
-            <p className="text-sm text-gray-600 ">
+            <p className="text-sm text-[#7e7e7e] ">
                 Don't have an account?{' '}
                 <a href="/register" className="text-blue-500 hover:underline">
                     Register now
