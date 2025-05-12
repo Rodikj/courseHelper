5c41b6bd89c51a433c22d940bcc674d8a332e541
DoneDonev <donedonev15@gmail.com>
2025-05-12 20:03:04 +0200

    Added dashboard route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,15 +5,20 @@ import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import RootLayout from './layout/RootLayout'
+import Dashboard from './pages/Dashboard'
+import CoursePage from './pages/CoursePage'
 
 function App() {
   const router = createBrowserRouter(createRoutesFromElements(
-    <Route path='/' element={<RootLayout />}>
-      <Route index element={<Home />}/>
-      <Route path='register' element={<Register />} />
-      <Route path='login' element={<Login />} />
-    </Route>
-
+    <>
+      <Route path='/' element={<RootLayout />}>
+        <Route index element={<Home />}/>
+        <Route path='register' element={<Register />} />
+        <Route path='login' element={<Login />} />
+      </Route>
+      <Route path='dashboard' element={<Dashboard />} />
+      <Route path='course' element={<CoursePage />} />
+    </>
   ))
 
   return (
